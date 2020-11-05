604ae10361bd33f4c35d73c5bd3ce0fa42001309
Chris Kim <chriskim06@gmail.com>
2020-11-05 07:23:16 -0800

    Try adding istio

--- main.go
+++ main.go
@@ -7,69 +7,102 @@ import (
 	"time"
 
 	"github.com/blend/go-sdk/env"
-	"github.com/blend/go-sdk/ex"
-	"github.com/blend/go-sdk/graceful"
+	"github.com/blend/go-sdk/exception"
 	"github.com/blend/go-sdk/logger"
 	"github.com/blend/go-sdk/web"
 )
 
 func main() {
-	log := logger.All(logger.OptPath("echo"))
+	log := logger.All()
 
 	appStart := time.Now()
 
-	app := web.New(web.OptConfigFromEnv(), web.OptLog(log))
+	app := web.NewFromConfig(web.NewConfigFromEnv()).WithLogger(log)
 	app.GET("/", func(r *web.Ctx) web.Result {
-		return web.Text.Result("echo")
+		return r.Text().Result("echo")
 	})
 	app.GET("/headers", func(r *web.Ctx) web.Result {
-		contents, err := json.Marshal(r.Request.Header)
+		contents, err := json.Marshal(r.Request().Header)
 		if err != nil {
-			return r.Views.InternalError(err)
+			return r.View().InternalError(err)
 		}
-		return web.Text.Result(string(contents))
+		return r.Text().Result(string(contents))
 	})
 	app.GET("/env", func(r *web.Ctx) web.Result {
-		return web.JSON.Result(env.Env().Vars())
+		return r.JSON().Result(env.Env().Vars())
 	})
 	app.GET("/error", func(r *web.Ctx) web.Result {
-		return web.JSON.InternalError(ex.New("This is only a test", ex.OptMessagef("this is a message"), ex.OptInner(ex.New("inner exception"))))
+		return r.JSON().InternalError(exception.New("This is only a test").WithMessagef("this is a message").WithInner(exception.New("inner exception")))
+	})
+	app.GET("/proxy/*filepath", func(r *web.Ctx) web.Result {
+		return r.JSON().Result("OK!")
 	})
 	app.GET("/status", func(r *web.Ctx) web.Result {
 		if time.Since(appStart) > 12*time.Second {
-			return web.Text.Result("OK!")
+			return r.Text().Result("OK!")
 		}
-		return web.Text.InternalError(fmt.Errorf("not ready"))
+		return r.Text().InternalError(fmt.Errorf("not ready"))
 	})
 
 	app.GET("/long/:seconds", func(r *web.Ctx) web.Result {
 		seconds, err := web.IntValue(r.RouteParam("seconds"))
 		if err != nil {
-			return web.Text.BadRequest(err)
+			return r.Text().BadRequest(err)
 		}
 
-		start := time.Now()
-		r.Response.WriteHeader(http.StatusOK)
+		r.Response().WriteHeader(http.StatusOK)
 		timeout := time.After(time.Duration(seconds) * time.Second)
 		ticker := time.NewTicker(500 * time.Millisecond)
 		for {
 			select {
 			case <-ticker.C:
 				{
-					fmt.Fprintf(r.Response, "%v tick\n", time.Since(start))
-					r.Response.Flush()
+					fmt.Fprintf(r.Response(), "tick\n")
+					r.Response().InnerResponse().(http.Flusher).Flush()
 				}
 			case <-timeout:
 				{
-					fmt.Fprintf(r.Response, "timeout\n")
-					r.Response.Flush()
+					fmt.Fprintf(r.Response(), "timeout\n")
+					r.Response().InnerResponse().(http.Flusher).Flush()
 					return nil
 				}
 			}
 		}
 	})
 
-	if err := graceful.Shutdown(app); err != nil {
-		logger.FatalExit(err)
+	app.GET("/echo/*filepath", func(r *web.Ctx) web.Result {
+		body := r.Request().URL.Path
+		if len(body) == 0 {
+			return r.RawWithContentType(web.ContentTypeText, []byte("no response."))
+		}
+		return r.RawWithContentType(web.ContentTypeText, []byte(body))
+	})
+	app.POST("/echo/*filepath", func(r *web.Ctx) web.Result {
+		body, err := r.PostBody()
+		if err != nil {
+			return r.JSON().InternalError(err)
+		}
+		if len(body) == 0 {
+			return r.RawWithContentType(web.ContentTypeText, []byte("nada."))
+		}
+		return r.RawWithContentType(web.ContentTypeText, body)
+	})
+
+	app.WithMethodNotAllowedHandler(func(r *web.Ctx) web.Result {
+		log.Infof("headers: %#v", r.Request().Header)
+		body, _ := r.PostBodyAsString()
+		log.Infof("body: %s", body)
+		return r.JSON().OK()
+	})
+
+	app.WithNotFoundHandler(func(r *web.Ctx) web.Result {
+		log.Infof("headers: %#v", r.Request().Header)
+		body, _ := r.PostBodyAsString()
+		log.Infof("body: %s", body)
+		return r.JSON().OK()
+	})
+
+	if err := web.StartWithGracefulShutdown(app); err != nil {
+		log.SyncFatalExit(err)
 	}
 }
